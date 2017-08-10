465d171311c4fff9f8c619cbc1847ca9218c9319
Andrey Stepachev <octo47@gmail.com>
2017-08-10 11:58:03 +0100

    Update protobuf@2.4.rb

--- Formula/protobuf@2.4.rb
+++ Formula/protobuf@2.4.rb
@@ -11,7 +11,7 @@ class ProtobufAT24 < Formula
     [['--universal', 'Do a universal build']]
   end
 
-  def patch
+  def patch do
     url 'https://github.com/octo47/homebrew-formula/raw/master/Patches/protobuf%402.4.patch'
     sha256 '04b9a8480381c2681fe9e292b3dd6f140f8a1e9e9167f97d0393bb80361d1b6a'
   end
