70df88b20ac0ce349b12d859e56a85ddc44168a0
Andrey Stepachev <octo47@gmail.com>
2017-08-10 11:57:02 +0100

    Update protobuf@2.4.rb

--- Formula/protobuf@2.4.rb
+++ Formula/protobuf@2.4.rb
@@ -11,6 +11,11 @@ class ProtobufAT24 < Formula
     [['--universal', 'Do a universal build']]
   end
 
+  def patch
+    url 'https://github.com/octo47/homebrew-formula/raw/master/Patches/protobuf%402.4.patch'
+    sha256 '04b9a8480381c2681fe9e292b3dd6f140f8a1e9e9167f97d0393bb80361d1b6a'
+  end
+  
   def install
     ENV.universal_binary if ARGV.build_universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
